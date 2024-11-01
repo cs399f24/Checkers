f8c013f9513d32964bbd47e054a17d15af80edd9
Jeffery Eisenhardt <eisenhardtj@moravian.edu>
2024-11-01 09:26:44 -0400

    Added more documentation and fixed it so it should work now

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,15 @@
 
 #update and install necessary packages
 sudo yum update -y
-
 yum install -y git
-git clone https://github.com/cs399f24/Checkers.git
-cd Checkers
+
+# Clones the repository and cd's into it
+git clone https://github.com/cs399f24/Checkers.git && cd Checkers
+
+# Creates the virtual environment and installs the requirements
 python3 -m venv .venv
-.venv/bin/pip install -r requirements.txt
+source .venv/bin/activate
+pip install -r requirements.txt
+
+# Runs the app.py
+python app.py
