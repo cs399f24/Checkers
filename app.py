0815b1aa2b7539b12d267373a42c9902d7ed3e77
Jeffery Eisenhardt <125985491+eisenhardtj@users.noreply.github.com>
2024-11-06 18:40:47 -0500

    Added env functionality

--- app.py
+++ app.py
@@ -82,6 +82,15 @@ def on_disconnect():
         players.remove(request.sid)
     emit('reset', room=room_id)
 
+load_dotenv()
+
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+    region_name=os.getenv('AWS_REGION')
+)
+
 if __name__ == '__main__':
     app.run(port=8080, host='0.0.0.0')
     socketio.run(app, host="0.0.0.0", port=8080)
