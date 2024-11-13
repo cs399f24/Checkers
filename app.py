824260ce888d78abdc50f6e83b9305339b05c3d8
Brandon Yang <brandon.yang1210@gmail.com>
2024-11-12 20:45:51 -0500

    corrected the possible path for index

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ socketio = SocketIO(app)
 # Home route
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('/templates/index.html')
 
 @socketio.on('connect')
 def handle_connect():
