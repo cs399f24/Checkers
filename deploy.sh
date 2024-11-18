e91cae9bff1a76b7c6759f77b07380f00848a1b7
Nathaniel Hajel <nhajel17@gmail.com>
2024-11-17 21:44:43 -0500

    Big push. Brought stuff from development onto main. Congito starting code. Added a new page for starting a game. added a login page

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,10 @@
 
 #update and install necessary packages
 sudo yum update -y
-sudo yum install -y git python3
+yum install -y git
 
 # Clones the repository and cd's into it
-git clone https://github.com/cs399f24/Checkers.git 
-cd Checkers
+git clone https://github.com/cs399f24/Checkers.git && cd Checkers
 
 # Creates the virtual environment and installs the requirements
 python3 -m venv .venv
