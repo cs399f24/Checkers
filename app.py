208df85c5ed5cf8d24befb4cf6e7ff03c5cf117c
Brandon Yang <brandon.yang1210@gmail.com>
2024-11-20 20:59:50 -0500

    fixing imports

--- app.py
+++ app.py
@@ -48,5 +48,5 @@ def handle_connect():
 def handle_disconnect():
     print("Client disconnected")
 
-if __name == '__main':
+if __name__ == '__main':
     socketio.run(app, debug=True, host='0.0.0.0', port=8080)
